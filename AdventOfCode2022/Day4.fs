61a036b875308b9d8f4da4baffe70f180aac148a
Smaug123 <patrick+github@patrickstevens.co.uk>
2022-12-03 21:48:58 +0000

    Day 4 skeleton

---
+++ AdventOfCode2022/Day4.fs
@@ -0,0 +1,18 @@
+namespace AdventOfCode2022
+
+[<RequireQualifiedAccess>]
+module Day4 =
+
+    let part1 (lines : string seq) : int =
+        lines
+        |> Seq.map (fun s ->
+            failwith "implement"
+        )
+        |> Seq.sum
+
+    let part2 (lines : string seq) : int =
+        lines
+        |> Seq.map (fun s ->
+            failwith "implement"
+        )
+        |> Seq.sum
