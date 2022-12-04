153714b08680c657cf177f92df4961f2aa4216a8
Smaug123 <patrick+github@patrickstevens.co.uk>
2022-12-04 08:53:06 +0000

    Day 4

--- AdventOfCode2022.Test/Day4.fs
+++ AdventOfCode2022.Test/Day4.fs
@@ -10,29 +10,43 @@ module TestDay4 =
 
     [<Test>]
     let ``Part 1, given`` () =
-        """A Y
-B X
-C Z"""
+        """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"""
         |> fun s -> s.Split System.Environment.NewLine
         |> Day4.part1
-        |> shouldEqual 15
+        |> shouldEqual 2
 
     [<Test>]
     let ``Part 1`` () =
         let input = Assembly.readResource "Day4.txt"
-        input.Split '\n' |> Day4.part1 |> shouldEqual 9651
+
+        input.Split '\n'
+        |> Seq.filter (not << String.IsNullOrWhiteSpace)
+        |> Day4.part1
+        |> shouldEqual 433
 
 
     [<Test>]
     let ``Part 2, given`` () =
-        """A Y
-B X
-C Z"""
+        """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"""
         |> fun s -> s.Split System.Environment.NewLine
         |> Day4.part2
-        |> shouldEqual 12
+        |> shouldEqual 4
 
     [<Test>]
     let ``Part 2`` () =
         let input = Assembly.readResource "Day4.txt"
-        input.Split '\n' |> Day4.part2 |> shouldEqual 10560
+
+        input.Split '\n'
+        |> Seq.filter (not << String.IsNullOrWhiteSpace)
+        |> Day4.part2
+        |> shouldEqual 852
