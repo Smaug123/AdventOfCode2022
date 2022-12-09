25fb305bea1e446505dfa2f7f2d121e2db03257c
Patrick Stevens <patrick+github@patrickstevens.co.uk>
2022-12-09 10:55:02 +0000

    Big perf improvements (#11)

--- AdventOfCode2022/Day6.fs
+++ AdventOfCode2022/Day6.fs
@@ -17,15 +17,67 @@ module Day6 =
 
         result
 
-    let rec private go (count : int) (s : ReadOnlySpan<char>) (i : int) =
-        let fourChars = s.Slice (i - count + 1, count)
-        let arr = fourChars.ToArray ()
-        Array.sortInPlace arr
+    let private swapFor (arr : char[]) (toRemove : char) (toInsert : char) : unit =
+        let index = Array.IndexOf (arr, toRemove)
+
+        if
+            (index = 0 || arr.[index - 1] <= toInsert)
+            && (index = arr.Length - 1 || toInsert <= arr.[index + 1])
+        then
+            arr.[index] <- toInsert
+        else
+
+        let mutable i = index
+        let mutable notInserted = true
+
+        if (index = 0 || arr.[index - 1] <= toInsert) then
+            // Inserting above
+            if toInsert > toRemove then
+                while notInserted && i < arr.Length - 1 do
+                    arr.[i] <- arr.[i + 1]
+                    i <- i + 1
+
+                    if i = arr.Length - 1 || toInsert <= arr.[i + 1] then
+                        arr.[i] <- toInsert
+                        notInserted <- false
+
+                if notInserted then
+                    arr.[arr.Length - 1] <- toInsert
+
+        else
+            // Inserting below
+            while notInserted && i > 0 do
+                arr.[i] <- arr.[i - 1]
+                i <- i - 1
+
+                if i = 0 || toInsert >= arr.[i - 1] then
+                    arr.[i] <- toInsert
+                    notInserted <- false
+
+            if notInserted then
+                arr.[0] <- toInsert
+
+    let rec private go (arr : char[]) (count : int) (s : ReadOnlySpan<char>) (i : int) =
+        if s.[i - count + 1] = s.[i + 1] then
+            go arr count s (i + 1)
+        else
+
+        swapFor arr s.[i - count + 1] s.[i + 1]
 
         match findDuplicateSorted arr with
-        | ValueSome _ -> go count s (i + 1)
-        | ValueNone -> i + 1
+        | ValueSome _ -> go arr count s (i + 1)
+        | ValueNone -> i + 2
 
-    let part1 (line : string) : int = go 4 (line.AsSpan ()) 3
+    let part1 (line : string) : int =
+        let length = 4
+        let line = line.AsSpan ()
+        let arr = line.Slice(0, length).ToArray ()
+        Array.sortInPlace arr
+        go arr length line (length - 1)
 
-    let part2 (line : string) : int = go 14 (line.AsSpan ()) 13
+    let part2 (line : string) : int =
+        let length = 14
+        let line = line.AsSpan ()
+        let arr = line.Slice(0, length).ToArray ()
+        Array.sortInPlace arr
+        go arr length line (length - 1)
