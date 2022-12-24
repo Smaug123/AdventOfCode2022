69a7a8ac009836a53fe964f4f6a53aad940f89f2
Smaug123 <patrick+github@patrickstevens.co.uk>
2022-12-24 12:39:22 +0000

    Better data structure

--- AdventOfCode2022/Day24.fs
+++ AdventOfCode2022/Day24.fs
@@ -18,12 +18,13 @@ module Day24 =
     // byte % 4 is whether Down is in;
     // byte % 8 is whether Left is in;
     // byte % 16 is whether Right is in.
-    type Day24Board = ImmutableDictionary<Coordinate, byte>
+    // This is logically a 2D array, but without having to give up ownership.
+    type Day24Board = byte[]
 
-    /// Returns the width and the height too.
-    let parse (line : StringSplitEnumerator) : Day24Board * int * int =
+    /// Returns the width and the height too. The resulting array is suitable to become an Arr2D.
+    let parse (line : StringSplitEnumerator) : byte[] * int * int =
         use mutable enum = line.GetEnumerator ()
-        let output = Dictionary ()
+        let output = ResizeArray ()
         let mutable y = 0
         let mutable width = 0
 
@@ -39,12 +40,12 @@ module Day24 =
                         }
 
                     match c with
-                    | '>' -> output.Add (coord, 8uy)
-                    | '^' -> output.Add (coord, 1uy)
-                    | 'v' -> output.Add (coord, 2uy)
-                    | '<' -> output.Add (coord, 4uy)
+                    | '>' -> output.Add 8uy
+                    | '^' -> output.Add 1uy
+                    | 'v' -> output.Add 2uy
+                    | '<' -> output.Add 4uy
                     | '.'
-                    | '#' -> ()
+                    | '#' -> output.Add 0uy
                     | _ -> failwithf "unexpected char: %c" c
 
                     x <- x + 1
@@ -53,44 +54,69 @@ module Day24 =
 
                 y <- y + 1
 
-        output.ToImmutableDictionary (), width, y
+        output.ToArray (), width, y
 
     let moveBlizzards (width : int) (height : int) (board : Day24Board) : Day24Board =
-        board
-        |> Seq.collect (fun (KeyValue (position, directions)) ->
-            seq {
+#if DEBUG
+        let board =
+            {
+                Elements = board
+                Width = width
+            }
+#else
+        use boardPtr = fixed board
+
+        let board =
+            {
+                Elements = boardPtr
+                Width = width
+                Length = width * height
+            }
+#endif
+
+        let resultArr = Array.zeroCreate<byte> (width * height)
+#if DEBUG
+        let result =
+            {
+                Elements = resultArr
+                Width = width
+            }
+#else
+        use ptr = fixed resultArr
+
+        let result =
+            {
+                Elements = ptr
+                Width = width
+                Length = resultArr.Length
+            }
+#endif
+
+        for y = 1 to height - 2 do
+            for x = 1 to width - 2 do
+                let directions = Arr2D.get board x y
+
                 if directions % 2uy = 1uy then
-                    yield
-                        { position with
-                            Y = if position.Y = 1 then height - 2 else position.Y - 1
-                        },
-                        1uy
+                    let y = if y = 1 then height - 2 else y - 1
+                    let prev = Arr2D.get result x y
+                    Arr2D.set result x y (prev + 1uy)
 
                 if (directions / 2uy) % 2uy = 1uy then
-                    yield
-                        { position with
-                            Y = if position.Y = height - 2 then 1 else position.Y + 1
-                        },
-                        2uy
+                    let y = if y = height - 2 then 1 else y + 1
+                    let prev = Arr2D.get result x y
+                    Arr2D.set result x y (prev + 2uy)
 
                 if (directions / 4uy) % 2uy = 1uy then
-                    yield
-                        { position with
-                            X = if position.X = 1 then width - 2 else position.X - 1
-                        },
-                        4uy
+                    let x = if x = 1 then width - 2 else x - 1
+                    let prev = Arr2D.get result x y
+                    Arr2D.set result x y (prev + 4uy)
 
                 if (directions / 8uy) % 2uy = 1uy then
-                    yield
-                        { position with
-                            X = if position.X = width - 2 then 1 else position.X + 1
-                        },
-                        8uy
-            }
-        )
-        |> Seq.groupBy fst
-        |> Seq.map (fun (coord, directions) -> KeyValuePair (coord, Seq.sum (Seq.map snd directions)))
-        |> ImmutableDictionary.CreateRange
+                    let x = if x = width - 2 then 1 else x + 1
+                    let prev = Arr2D.get result x y
+                    Arr2D.set result x y (prev + 8uy)
+
+        resultArr
 
     let boardAtTime (width : int) (height : int) (startBoard : Day24Board) : int -> Day24Board =
         let store = ResizeArray ()
@@ -109,44 +135,45 @@ module Day24 =
         (width : int)
         (height : int)
         (current : Coordinate)
-        (board : Day24Board)
+        (board : Arr2D<byte>)
         : Coordinate list
         =
         [
             if current.X > 1 && current.Y <> 0 && current.Y <> height - 1 then
-                let trial =
-                    { current with
-                        X = current.X - 1
-                    }
+                if Arr2D.get board (current.X - 1) current.Y = 0uy then
+                    let trial =
+                        { current with
+                            X = current.X - 1
+                        }
 
-                if not (board.ContainsKey trial) then
                     yield trial
+            if Arr2D.get board current.X current.Y = 0uy then
+                yield current
             if current.X < width - 2 && current.Y <> 0 then
-                let trial =
-                    { current with
-                        X = current.X + 1
-                    }
+                if Arr2D.get board (current.X + 1) current.Y = 0uy then
+                    let trial =
+                        { current with
+                            X = current.X + 1
+                        }
 
-                if not (board.ContainsKey trial) then
                     yield trial
             if current.Y > 1 && current.X <> 0 then
-                let trial =
-                    { current with
-                        Y = current.Y - 1
-                    }
 
-                if not (board.ContainsKey trial) then
+                if Arr2D.get board current.X (current.Y - 1) = 0uy then
+                    let trial =
+                        { current with
+                            Y = current.Y - 1
+                        }
+
                     yield trial
             if current.Y < height - 2 && current.X <> 0 then
-                let trial =
-                    { current with
-                        Y = current.Y + 1
-                    }
+                if Arr2D.get board current.X (current.Y + 1) = 0uy then
+                    let trial =
+                        { current with
+                            Y = current.Y + 1
+                        }
 
-                if not (board.ContainsKey trial) then
                     yield trial
-            if not (board.ContainsKey current) then
-                yield current
         ]
 
     let availableMoves
@@ -161,6 +188,23 @@ module Day24 =
             match store.TryGetValue args with
             | false, _ ->
                 let board = boardAtTime (timeStep + 1)
+
+#if DEBUG
+                let board =
+                    {
+                        Elements = board
+                        Width = width
+                    }
+#else
+                use ptr = fixed board
+
+                let board =
+                    {
+                        Elements = ptr
+                        Width = width
+                        Length = width * height
+                    }
+#endif
 
                 let afterMoving = availableIndividualMoves width height currPos board
 
