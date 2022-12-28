e5b9fecded3801618ab145b7291fa17b2d80c538
Smaug123 <patrick+github@patrickstevens.co.uk>
2022-12-28 10:58:08 +0000

    And workflow

--- flake.nix
+++ flake.nix
@@ -22,26 +22,28 @@
       dotnet-sdk = pkgs.dotnet-sdk_7;
       dotnet-runtime = pkgs.dotnetCorePackages.runtime_7_0;
       version = "0.0.1";
-    in {
-      packages = {
-        fantomas = pkgs.stdenvNoCC.mkDerivation rec {
-          name = "fantomas";
-          version = "5.2.0-alpha-008";
-          nativeBuildInputs = [ pkgs.makeWrapper ];
+      dotnetTool = toolName: toolVersion: sha256:
+        pkgs.stdenvNoCC.mkDerivation rec {
+          name = toolName;
+          version = toolVersion;
+          nativeBuildInputs = [pkgs.makeWrapper];
           src = pkgs.fetchNuGet {
             pname = name;
             version = version;
-            sha256 = "sha256-1egphbWXTjs2I5aFaWibFDKgu3llP1o32o1X5vab6v4=";
+            sha256 = sha256;
             installPhase = ''mkdir -p $out/bin && cp -r tools/net6.0/any/* $out/bin'';
           };
           installPhase = ''
             runHook preInstall
             mkdir -p "$out/lib"
             cp -r ./bin/* "$out/lib"
-            makeWrapper "${dotnet-runtime}/bin/dotnet" "$out/bin/fantomas" --add-flags "$out/lib/fantomas.dll"
+            makeWrapper "${dotnet-runtime}/bin/dotnet" "$out/bin/${name}" --add-flags "$out/lib/${name}.dll"
             runHook postInstall
           '';
         };
+    in {
+      packages = {
+        fantomas = dotnetTool "fantomas" "5.2.0-alpha-008" "sha256-1egphbWXTjs2I5aFaWibFDKgu3llP1o32o1X5vab6v4=";
         fetchDeps = let
           flags = [];
           runtimeIds = map (system: pkgs.dotnetCorePackages.systemToDotnetRid system) dotnet-sdk.meta.platforms;
@@ -81,6 +83,8 @@
         ];
         packages = [
           pkgs.alejandra
+          pkgs.nodePackages.markdown-link-check
+          pkgs.shellcheck
         ];
       };
     });
